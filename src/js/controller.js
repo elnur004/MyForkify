97a197e1b3f2f07e88f181b45ed517075362bdb1
elnur004 <lnur004@gmail.com>
2021-03-08 12:52:04 +0400

    looped over the ingredients

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,8 +16,8 @@ const showRecipe = async function () {
   try {
     // 1) Loading recipe
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc958'
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc958'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
 
     const data = await res.json();
@@ -41,7 +41,9 @@ const showRecipe = async function () {
     // 2) Rendering recipe
     const markup = `
         <figure class="recipe__fig">
-              <img src="${recipe.imageUrl}" alt=${recipe.title} class="recipe__img" />
+              <img src="${recipe.imageUrl}" alt=${
+      recipe.title
+    } class="recipe__img" />
               <h1 class="recipe__title">
                 <span>${recipe.title}</span>
               </h1>
@@ -52,14 +54,18 @@ const showRecipe = async function () {
           <svg class="recipe__info-icon">
             <use href="src/img/icons.svg#icon-clock"></use>
           </svg>
-          <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            recipe.cookingTime
+          }</span>
           <span class="recipe__info-text">minutes</span>
         </div>
         <div class="recipe__info">
           <svg class="recipe__info-icon">
             <use href="src/img/icons.svg#icon-users"></use>
           </svg>
-          <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+          <span class="recipe__info-data recipe__info-data--people">${
+            recipe.servings
+          }</span>
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
@@ -91,35 +97,31 @@ const showRecipe = async function () {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          <li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="src/img/icons.svg#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">1000</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">g</span>
-              pasta
-            </div>
-          </li>
-
-          <li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="src/img/icons.svg#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">0.5</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">cup</span>
-              ricotta cheese
-            </div>
-          </li>
-        </ul>
+          ${recipe.ingredients
+            .map(ing => {
+              return `
+            <li class="recipe__ingredient">
+                <svg class="recipe__icon">
+                  <use href="src/img/icons.svg#icon-check"></use>
+                </svg>
+                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__description">
+                  <span class="recipe__unit">${ing.unit}</span>
+                  ${ing.description}
+                </div>
+            </li>
+                `;
+            })
+            .join('')}
       </div>
 
       <div class="recipe__directions">
         <h2 class="heading--2">How to cook it</h2>
         <p class="recipe__directions-text">
           This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+          <span class="recipe__publisher">${
+            recipe.publisher
+          }</span>. Please check out
           directions at their website.
         </p>
         <a
@@ -134,7 +136,7 @@ const showRecipe = async function () {
         </a>
       </div>
     `;
-
+    recipeContainer.innerHTML = '';
     recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     alert(`${err}`);
