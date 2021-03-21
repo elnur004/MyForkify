3d9b992cc46d1927f5434260f76567404b922836
elnur004 <lnur004@gmail.com>
2021-03-21 18:45:23 +0400

    rendered list of search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,6 +1,7 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable'; // for polyfilling everything
 import 'regenerator-runtime/runtime'; // for polyfilling async/await
@@ -9,7 +10,6 @@ import 'regenerator-runtime/runtime'; // for polyfilling async/await
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -26,6 +26,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -34,6 +36,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
+    resultsView.render(model.state.search.results);
     console.log(model.state.search.results);
   } catch (err) {
     console.log(err);
