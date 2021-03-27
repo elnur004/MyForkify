4e14ffc8933cecf9f090febe873731c11a47b28c
elnur004 <lnur004@gmail.com>
2021-03-27 19:10:18 +0400

    Updated results view to mark selected search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,6 +20,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultPage());
+
     // 1) Loading recipe
     await model.loadRecipe(id);
 
