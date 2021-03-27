c78ea6e93d0c1e652dab2cf68e0dd301fd4db23b
elnur004 <lnur004@gmail.com>
2021-03-27 14:03:03 +0400

    added algorithm to update attributes

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -28,12 +28,18 @@ export default class View {
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
 
+      // Updates changed TEXT
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
       ) {
         curEl.textContent = newEl.textContent;
       }
+      // Updates changed ATTRIBUTES
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
     });
   }
 
