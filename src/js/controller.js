3183c080fc83d517373fcd5fd41dce14113fd92a
elnur004 <lnur004@gmail.com>
2021-04-08 03:16:39 +0400

    handled the error if the ingredient input was incorrect

--- src/js/controller.js
+++ src/js/controller.js
@@ -96,9 +96,14 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
-const controlAddRecipe = function (newRecipe) {
-  // Upload the new recipe data
-  model.uploadRecipe(newRecipe);
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 // Publisher-Subscriber Design pattern (Publiher: addHandlerRender, Subscriber: controlRecipes)
