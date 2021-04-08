accf28ac5d9d8abf530bdfc828f4579d34f6613f
elnur004 <lnur004@gmail.com>
2021-04-08 05:01:24 +0400

    implemented to send uploaded recipe to the fetch API

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '002ee243-cb0b-4831-8192-ad6347677945';
