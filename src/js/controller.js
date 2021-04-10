0e3e59165cdd47b6cb73c3a8d5793daae2833a3f
elnur004 <lnur004@gmail.com>
2021-04-10 07:38:44 +0400

    console message

--- src/js/controller.js
+++ src/js/controller.js
@@ -133,6 +133,10 @@ const controlAddRecipe = async function (newRecipe) {
   }, MODAL_CLOSE_SEC * 1000);
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 // Publisher-Subscriber Design pattern (Publiher: addHandlerRender, Subscriber: controlRecipes)
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -142,5 +146,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
