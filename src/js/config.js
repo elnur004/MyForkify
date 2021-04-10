cde1780f6899a313db024fea8c6814063a301188
elnur004 <lnur004@gmail.com>
2021-04-10 08:23:15 +0400

    changed API KEY

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '002ee243-cb0b-4831-8192-ad6347677945';
+export const KEY = 'c5fe2fe5-871f-42e6-903f-a1972cee5c4e';
 export const MODAL_CLOSE_SEC = 1.5;
